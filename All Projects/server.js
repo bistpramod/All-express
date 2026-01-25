7545c995405a915d7805ce8648e902744619c331
new divide <bistpramod113@gmail.com>
2026-01-25 23:22:41 +0545

    Fix CORS issue and add package.json for All Projects server

--- All Projects/server.js
+++ All Projects/server.js
@@ -10,6 +10,15 @@ const app = express()
 // Define the port number for the server to listen on
 const PORT = 4000
 
+// Middleware to enable CORS (Cross-Origin Resource Sharing)
+// This allows the API to be accessed from web browsers on different domains
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 // Define a GET route for '/users' that returns the list of users as JSON
 app.get('/users', (req, res) => {
   // Send the users data as a JSON response
